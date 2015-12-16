5df3909dc69d8f8bbc2e61d178e048d8f27ecd78
Josh Gillies <github@joshgilli.es>
2015-12-16 23:55:58 +1100

    Add additional tests

--- test.js
+++ test.js
@@ -9,8 +9,11 @@ test('basic conversion', function (assert) {
 
 test('specified output conversion', function (assert) {
   var test = 'color: rebeccapurple'
-  assert.equal(rebeccapurple(test, 'hex'), 'color: #663399')
-  assert.equal(rebeccapurple(test, 'hsl'), 'color: hsl(270, 50%, 40%)')
+  assert.equal(rebeccapurple(test, 'hex'), 'color: #663399', 'outputs hex color')
+  assert.equal(rebeccapurple(test, 'hsl'), 'color: hsl(270, 50%, 40%)', 'outputs hsl color')
+  assert.equal(rebeccapurple(test, 'hwb'), 'color: hwb(270, 20%, 40%)', 'outputs hwb color')
+  assert.equal(rebeccapurple(test, 'percent'), 'color: rgb(40%, 20%, 60%)', 'outputs rgb color with percentage values')
+  assert.equal(rebeccapurple(test, 'rgb'), 'color: rgb(102, 51, 153)', 'outputs rbg color')
   assert.end()
 })
 
