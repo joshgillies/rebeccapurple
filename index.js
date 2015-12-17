41b0c285be085761e4a49c9e1df3f30390f770db
Josh Gillies <github@joshgilli.es>
2015-12-17 20:26:13 +1100

    Add static color values

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var color = require('color')
 
+var COLORS = {
+  'hex': '#663399',
+  'hsl': 'hsl(270, 50%, 40%)',
+  'hwb': 'hwb(270, 20%, 40%)',
+  'percent': 'rgb(40%, 20%, 60%)',
+  'rgb': 'rgb(102, 51, 153)'
+}
+
 module.exports = function rebeccapurple (string, output) {
+  var value
+
   if (!output) {
     output = 'rgb'
   }
-  return string.replace(/(rebeccapurple)\b/gi, color('rebeccapurple')[output + 'String']())
+
+  if (output in COLORS) {
+    value = COLORS[output]
+  } else {
+    value = color('rebeccapurple')[output + 'String']()
+  }
+  return string.replace(/(rebeccapurple)\b/gi, value)
 }
